504b6272553dc05cc8563e8aa7020efb0b3f0f56
4slan <amalsago@student.42.fr>
2020-01-15 20:23:14 +0100

    adding main() function witch calls whois(); normalizing

--- day00/ex02/whois.py
+++ day00/ex02/whois.py
@@ -1,15 +1,25 @@
 import sys
 
-if len(sys.argv) == 1:
-    exit()
-elif len(sys.argv) > 2 or not sys.argv[1].isdigit():
-    print "ERROR"
-elif sys.argv[1].isdigit():
-    number = int(sys.argv[1])
-    if number == 0:
-        print "I'm Zero."
-    else:
-        if (number % 2) == 0:
-            print "I'm Even"
+
+def whois():
+    if len(sys.argv) == 1:
+        exit()
+    elif len(sys.argv) > 2 or not sys.argv[1].isdigit():
+        print("ERROR")
+    elif sys.argv[1].isdigit():
+        number = int(sys.argv[1])
+        if number == 0:
+            print("I'm Zero.")
         else:
-            print "I'm Odd"
+            if (number % 2) == 0:
+                print("I'm Even")
+            else:
+                print("I'm Odd")
+
+
+def main():
+    whois()
+
+
+if __name__ == '__main__':
+    main()
